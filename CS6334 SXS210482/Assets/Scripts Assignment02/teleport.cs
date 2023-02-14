346e4e117c7b5ca6659ad93a1b0dac8e136b521b
Shreeprasad Sonar <65328555+ShreeprasadSonar@users.noreply.github.com>
2023-02-13 18:03:41 -0600

    New changes

--- CS6334 SXS210482/Assets/Scripts Assignment02/teleport.cs
+++ CS6334 SXS210482/Assets/Scripts Assignment02/teleport.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class teleport : MonoBehaviour
+public class teleport : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public GameObject teleportTarget;
+    public GameObject player;
+    bool hasPointerEntered = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,22 @@ public class teleport : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (hasPointerEntered && Input.GetKey("joystick button 0"))
+        {
+            player.SetActive(false);
+            player.transform.position = teleportTarget.transform.position;
+            player.SetActive(true);
+            Destroy(teleportTarget);        
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hasPointerEntered = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hasPointerEntered = false;
     }
 }
